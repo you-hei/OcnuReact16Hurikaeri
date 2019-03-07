92bfcbeca29d36a41156809392b5c48fe081d0f3
you-hei <nakazato.2000@gmail.com>
2019-03-07 15:06:26 +0900

    react16.2一旦

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import React15NoYatsu from "./vasion-betsu/React15NoYatsu";
 import React16NoYatsu from "./vasion-betsu/React160NoYatsu";
+import React162NoYatsu from "./vasion-betsu/React162NoYatsu";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/*<React15NoYatsu />*/}
-        <React16NoYatsu />
+        {/*<React16NoYatsu />*/}
+        <React162NoYatsu />
       </div>
     );
   }
---
+++ src/vasion-betsu/React162NoYatsu.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const FragmentsDekiru = () => (
+    <React.Fragment>
+        <div>こ１</div>
+        <div>こ２</div>
+        <div>こ３</div>
+    </React.Fragment>
+)
+
+const FragmentsDekiru2 = () => (
+    <>
+        <div>こ2222１</div>
+        <div>こ2222２</div>
+        <div>こ2222３</div>
+    </>
+)
+
+const React162NoYatsu = () => (
+    <div>
+        <h1>React16.2</h1>
+        <h2>Fragments</h2>
+        <FragmentsDekiru />
+        {/*<FragmentsDekiru2 />*/}
+    </div>
+)
+
+export default React162NoYatsu
