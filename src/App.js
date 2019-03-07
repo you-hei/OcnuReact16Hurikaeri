8f382614e50ef5f5ccb8bc40f31a0fd6519fd062
you-hei <nakazato.2000@gmail.com>
2019-03-07 14:52:53 +0900

    React16.0系

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import React15NoYatsu from "./vasion-betsu/React15NoYatsu";
+import React16NoYatsu from "./vasion-betsu/React160NoYatsu";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <React15NoYatsu />
+        {/*<React15NoYatsu />*/}
+        <React16NoYatsu />
       </div>
     );
   }
---
+++ src/vasion-betsu/React160NoYatsu.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const FragmentsDekiru = () => [
+    <div key="boe1">こ１</div>,
+    <div key="boe2">こ２</div>,
+    <div key="boe3">こ３</div>,
+]
+
+const portalsRoot = document.getElementById('portals-root')
+
+class PortalsDekiru extends Component {
+    constructor(props) {
+        super(props)
+        this.el = document.createElement('div')
+    }
+    componentDidMount() {
+        portalsRoot.appendChild(this.el)
+    }
+    componentWillUnmount() {
+        portalsRoot.removeChild(this.el)
+    }
+
+    render() {
+        return ReactDOM.createPortal(
+            this.props.children,
+            this.el
+        )
+    }
+}
+
+class ErrorBoundariesDekiru extends Component {
+    render() {
+        return <div/>
+    }
+}
+
+const React16NoYatsu = () => (
+    <div>
+        <h1>React15</h1>
+        <h2>Fragments</h2>
+        <FragmentsDekiru />
+        <h2>Portals</h2>
+        <PortalsDekiru>ジョンダリ</PortalsDekiru>
+        <h2>Error Boundaries</h2>
+    </div>
+)
+
+export default React16NoYatsu
